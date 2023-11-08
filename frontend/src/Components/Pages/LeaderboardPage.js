650ee7b7bc52aee17c32a92883d9f34934bf3b03
xavier-massart-vinci <xavier.massart@student.vinci.be>
2023-11-08 10:25:11 +0100

    refactor: create render function

--- frontend/src/Components/Pages/LeaderboardPage.js
+++ frontend/src/Components/Pages/LeaderboardPage.js
@@ -1,38 +1,44 @@
 
+
+
 const Leaderboard = () => {
-    const main = document.querySelector('main');
-    const lignes = ` 
-    <tr>
-        <td class="text-center">1</td>
-        <td class="text-center">GoodHyper</td>
-        <td class="text-center">128 m</td>
-        <td class="text-center">10/10/1999</td>
-    </tr>`;
+  const lignes = ` 
+  <tr>
+      <td class="text-center">1</td>
+      <td class="text-center">GoodHyper</td>
+      <td class="text-center">128 m</td>
+      <td class="text-center">10/10/1999</td>
+  </tr>`;
+  renderLeaderboardPage(lignes);
+}
 
 
-    main.innerHTML = `
-    <div class="d-flex justify-content-end">
-      <div class="back mx-5 my-4 p-2 rounded border bg-primary-subtle">back</div>
+function renderLeaderboardPage(lignes) {
+  const main = document.querySelector('main');
+
+  main.innerHTML = `
+  <div class="d-flex justify-content-end">
+    <div class="back mx-5 my-4 p-2 rounded border bg-primary-subtle">back</div>
+  </div>
+ 
+
+  <div class="d-flex justify-content-center">
+    <div class="container p-2 border rounded">
+        <h2 class="text-center mb-2">LeaderBoard</h2>
+        <div class="table-responsive mx-4">
+          <table class="table table-striped mx-auto">
+            <tr>
+              <th class="text-center">n°</th>
+              <th class="text-center">username</th>
+              <th class="text-center">score</th>
+              <th class="text-center">date</th>
+            </tr>
+           ${lignes}
+          </table>
+        </div>
     </div>
-   
-
-    <div class="d-flex justify-content-center">
-      <div class="container p-2 border rounded">
-          <h2 class="text-center mb-2">LeaderBoard</h2>
-          <div class="table-responsive mx-4">
-            <table class="table table-striped mx-auto">
-              <tr>
-                <th class="text-center">n°</th>
-                <th class="text-center">username</th>
-                <th class="text-center">score</th>
-                <th class="text-center">date</th>
-              </tr>
-             ${lignes}
-            </table>
-          </div>
-      </div>
-    </div>
-    `;
+  </div>
+  `;
 }
 
 export default Leaderboard;
