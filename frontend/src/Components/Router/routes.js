919d9f6cb188cf4a007a8863d0e34ba45d1ba1e8
xavier-massart-vinci <xavier.massart@student.vinci.be>
2023-11-08 09:46:45 +0100

    init: leaderboard

--- frontend/src/Components/Router/routes.js
+++ frontend/src/Components/Router/routes.js
@@ -1,10 +1,12 @@
 import GamePage from '../Pages/GamePage';
 import HomePage from '../Pages/HomePage';
+import Leaderboard from '../Pages/LeaderboardPage';
 import NewPage from '../Pages/NewPage';
 
 const routes = {
   '/': HomePage,
   '/game': GamePage,
+  '/leaderboard': Leaderboard,
   '/new': NewPage,
 };
 
