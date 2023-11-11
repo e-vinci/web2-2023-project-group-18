318b7e3cf015a6d4b17fd5c4cdc61e09cf6bdc7e
Chisom <chisom.ubah@student.vinci.be>
2023-11-11 10:49:26 +0100

    set: RegisterPage

--- frontend/src/Components/Router/routes.js
+++ frontend/src/Components/Router/routes.js
@@ -1,9 +1,11 @@
 import GamePage from '../Pages/GamePage';
 import HomePage from '../Pages/HomePage';
 import NewPage from '../Pages/NewPage';
+import RegisterPage from '../Pages/RegisterPage';
 
 const routes = {
   '/': HomePage,
+  '/register': RegisterPage,
   '/game': GamePage,
   '/new': NewPage,
 };
