c1a574e23e54004036e36a32df8beb5373bc52b0
Alexis <alexis.cattaruzza@student.vinci.be>
2023-11-13 12:25:37 +0100

    modifying button and adding titleGame

--- frontend/src/Components/Game/StartMenuScene.js
+++ frontend/src/Components/Game/StartMenuScene.js
@@ -43,16 +43,41 @@ class StartMenuScene extends Phaser.Scene {
     const backgroundShadow = this.add.rectangle(400, 300, 800, 600, 0x000000, 0.5);
     backgroundShadow.setOrigin(0.5);
 
+    const titleGame = this.add.text(400,50,'SANTA FALL', {
+      fontSize: '50px',
+      fontFamily: 'Georgia Pro Light',
+      color: '#ffffff'
+    })
+    titleGame.setOrigin(0.5);
+
     this.playButton = this.add.image(400, 300, PLAY_BUTTON);
     this.playButton.setOrigin(0.5);
     this.playButton.setInteractive({ useHandCursor: true }); 
+
+    // Set up the pulsing animation
+    this.tweens.add({
+      targets: this.playButton,
+      scaleX: 1.2, // Scale factor for X
+      scaleY: 1.2, // Scale factor for Y
+      duration: 800, // Duration in milliseconds
+      yoyo: true, // Make the animation reverse to its original state
+      repeat: -1, // Repeat indefinitely
+    });
+
     this.playButton.on('pointerdown', () => {
       this.startGame();
-    });    
+    });   
+    this.playButton.on('pointerover',() => {
+      this.playButton.setTint(0xfc7d7d);
+    }) 
+    this.playButton.on('pointerout', () => {
+      this.playButton.clearTint();
+    })
 
     const textRules = this.add.text(400,410,'Press space to jump over obstacles and roll down the slope \n at full speed to earn as many coins as possible',{
-      fontSize: '20px',
-      color: '#ffffff'
+      fontSize: '23px',
+      color: '#ffffff',
+      fontFamily: 'Georgia Pro Light'
     });
     textRules.setOrigin(0.5);
     textRules.setAlign('center');
