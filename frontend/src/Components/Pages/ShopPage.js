a95ff2fae6b8129b2351ee6be630c248cd1df766
devos <thibaut.devos@student.vinci.be>
2023-11-14 14:16:41 +0100

    Change CSS Store

--- frontend/src/Components/Pages/ShopPage.js
+++ frontend/src/Components/Pages/ShopPage.js
@@ -18,17 +18,17 @@ const skinsList = [
 ];
 
 const themesList = [
-    { name: "Neige", price: 100 },
-    { name: "Prairie", price: 200 },
-    { name: "Désert", price: 300 },
-    { name: "Taïga", price: 400 },
-    { name: "Forêt", price: 500 },
-    { name: "Toundra", price: 600 },
-    { name: "Océan", price: 700 },
-    { name: "Marais", price: 800 },
-    { name: "Montagne", price: 900 },
-    { name: "Plaine", price: 1000 },
-    { name: "Roche", price: 1250 },
+    { name: "Snow", price: 100 },
+    { name: "Meadow", price: 200 },
+    { name: "Desert", price: 300 },
+    { name: "Taiga", price: 400 },
+    { name: "Forest", price: 500 },
+    { name: "Tundra", price: 600 },
+    { name: "Ocean", price: 700 },
+    { name: "Swamp", price: 800 },
+    { name: "Mountain", price: 900 },
+    { name: "Plain", price: 1000 },
+    { name: "Rock", price: 1250 },
     { name: "Jungle", price: 1500 }
 ];
 
@@ -37,13 +37,16 @@ const themesPerPage = 6;
 let currentSkinPage = 1;
 let currentThemePage = 1;
 
+const ownedSkins = ["dragon", "viper", "hydra", "wyvern"];
+const ownedThemes = ["snow", "meadow", "forest", "plain", "rock"];
+
 const ShopPage = () => {
     const numberCoins = 458; 
 
     renderShopPage(numberCoins);
-    
-    displayCurrentSkinPage();
-    displayCurrentThemePage();
+
+    displayCurrentSkinPage(ownedSkins);
+    displayCurrentThemePage(ownedThemes);
 
     changePageListenner();
     backListenner();
@@ -53,7 +56,8 @@ function renderShopPage(numberCoins) {
     const main = document.querySelector('main');
 
     const mainContent = `
-    <div>
+    <div id="shop-page">
+        <br>
         <div class="d-flex justify-content-between align-items-center position-relative m-3">
             <h1 class="mx-auto">Store</h1>
             <button type="button" class="back btn btn-primary btn-lg position-absolute top-0 end-0">Back</button>
@@ -93,14 +97,14 @@ function renderShopPage(numberCoins) {
                 </div>
             </div>
         </div>
-
+        <br>
     </div>
     `
 
     main.innerHTML = mainContent;
 }
 
-function displayCurrentSkinPage() {
+function displayCurrentSkinPage(ownSkins) {
     const startIndex = (currentSkinPage - 1) * skinsPerPage;
     const endIndex = startIndex + skinsPerPage;
     const currentSkins = skinsList.slice(startIndex, endIndex);
@@ -116,23 +120,28 @@ function displayCurrentSkinPage() {
         for (let j = i; j < i+3 && j < currentSkins.length; j+= 1) {
             const skin = currentSkins[j];
 
+            let typeButton;
+            if (ownSkins.includes(skin.name.toLowerCase()))
+                typeButton = `<button type="button" class="btn store-own-button">Choose</button>`;
+            else
+                typeButton = `<button type="button" class="btn store-buy-button">${skin.price} coins</button>`;
+
             skinHTML += `
                 <div class="col-md-4 text-center">
                     <h3>${skin.name}</h3>
-                    <img class="w-100 rounded" src="${templateSkinImage}" alt="Image du skin ${skin.name}">
-                    <button type="button" class="btn btn-primary">${skin.price} coins</button>
-                </div>
-            `;
+                    <img class="w-100 store-picture" src="${templateSkinImage}" alt="skin picture ${skin.name}">
+                    ${typeButton}
+                </div>`;
         }
 
-        skinHTML += '</div>';
+        skinHTML += '</div><br>';
     }
 
     skinPageNumber.innerHTML = `< ${currentSkinPage} > on < ${Math.ceil(skinsList.length/skinsPerPage)} >`;
     skinListPage.innerHTML = skinHTML;
 }
 
-function displayCurrentThemePage() {
+function displayCurrentThemePage(ownThemes) {
     const startIndex = (currentThemePage - 1) * themesPerPage;
     const endIndex = startIndex + themesPerPage;
     const currentThemes = themesList.slice(startIndex, endIndex);
@@ -148,16 +157,21 @@ function displayCurrentThemePage() {
         for (let j = i; j < i+3 && j < currentThemes.length; j+= 1) {
             const theme = currentThemes[j];
 
+            let typeButton;
+            if (ownThemes.includes(theme.name.toLowerCase()))
+                typeButton = `<button type="button" class="btn store-own-button">Choose</button>`;
+            else
+                typeButton = `<button type="button" class="btn store-buy-button">${theme.price} coins</button>`;
+
             themeHTML += `
                 <div class="col-md-4 text-center">
                     <h3>${theme.name}</h3>
-                    <img class="w-100 rounded" src="${templateMapImage}" alt="Image du theme ${theme.name}">
-                    <button type="button" class="btn btn-primary">${theme.price} coins</button>
-                </div>
-            `;
+                    <img class="w-100 store-picture" src="${templateMapImage}" alt="theme picture ${theme.name}">
+                    ${typeButton}
+                </div>`;
         }
 
-        themeHTML += '</div>';
+        themeHTML += '</div><br>';
     }
 
     themePageNumber.innerHTML = `< ${currentThemePage} > on < ${Math.ceil(themesList.length/themesPerPage)} >`;
@@ -168,28 +182,28 @@ function changePageListenner() {
     document.querySelector('#next-change-skin-page').addEventListener('click', () => {
         if (currentSkinPage < skinsList.length/skinsPerPage) {
             currentSkinPage += 1;
-            displayCurrentSkinPage();
+            displayCurrentSkinPage(ownedSkins);
         }
     });
 
     document.querySelector('#previous-change-skin-page').addEventListener('click', () => {
         if (currentSkinPage > 1) {
             currentSkinPage -= 1;
-            displayCurrentSkinPage();
+            displayCurrentSkinPage(ownedSkins);
         }
     });
 
     document.querySelector('#next-change-theme-page').addEventListener('click', () => {
         if (currentThemePage < themesList.length/themesPerPage) {
             currentThemePage += 1;
-            displayCurrentThemePage();
+            displayCurrentThemePage(ownedThemes);
         }
     });
 
     document.querySelector('#previous-change-theme-page').addEventListener('click', () => {
         if (currentThemePage > 1) {
             currentThemePage -= 1;
-            displayCurrentThemePage();
+            displayCurrentThemePage(ownedThemes);
         }
     });
 }
