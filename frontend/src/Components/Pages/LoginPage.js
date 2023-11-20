99ae5e9cd38a5f6d823b8ef1391b65e67918a129
Chisom <chisom.ubah@student.vinci.be>
2023-11-21 00:40:12 +0100

    add:animation + color

--- frontend/src/Components/Pages/LoginPage.js
+++ frontend/src/Components/Pages/LoginPage.js
@@ -1,8 +1,12 @@
-import Navigate from "../Router/Navigate";
+import anime from 'animejs/lib/anime.es';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import 'boxicons';
+
+import Navigate from '../Router/Navigate';
 
 const LoginPage = () => {
   const main = document.querySelector('main');
-   main.innerHTML = `
+  main.innerHTML = `
    <div class="div-back">
         <button id="link" class="back" type="button" data-uri="/"><i class='bx bx-arrow-back'></i></button>
       </div>
@@ -12,11 +16,12 @@ const LoginPage = () => {
             <h1>Login</h1>
             <div class="input-box">
               <input type="text" placeholder="Username" required>
-              <i class='bx bxs-user'></i>
+              <i class="bi bi-people"></i>
             </div>
+
             <div class="input-box">
               <input type="password" placeholder="Password" required>
-              <i class='bx bxs-lock-alt'></i>
+              <i class="bi bi-key-fill"></i>
             </div>
 
             <button type="submit" class="btn">Login</button>
@@ -27,17 +32,64 @@ const LoginPage = () => {
           </form>
         </div>
       </div>`;
-   
-   linkClick();
+  
+
+  linkClick();
+  document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    animeLogin(true);
+  });
 };
 
 function linkClick() {
-   const links = document.querySelectorAll('#link');
-   links.forEach((link) => {
-      link.addEventListener('click',()=>{
-         Navigate(link.dataset.uri);
-      });
-   });
+  const links = document.querySelectorAll('#link');
+  links.forEach((link) => {
+    link.addEventListener('click', () => {
+      Navigate(link.dataset.uri);
+    });
+  });
+}
+
+function animeLogin(isConnected) {
+  const borderColor = document.querySelector('.wrapper');
+  const inputsColor = document.querySelectorAll('.input-box input');
+  
+  if (isConnected) {
+    inputsColor.forEach((input) => {
+      // eslint-disable-next-line no-param-reassign
+      input.style.borderColor = '#00FF00';
+      // eslint-disable-next-line no-param-reassign
+      input.style.animationName = 'changeColorGreen';
+      // eslint-disable-next-line no-param-reassign
+      input.style.animationDuration = '2s';
+
+    });
+    
+    borderColor.style.borderColor = '#00FF00';
+    borderColor.style.animationName = 'changeColorGreen';
+    borderColor.style.animationDuration = '2s';
+  }
+    else {
+    borderColor.style.borderColor = '#FF0000';
+    borderColor.style.animationName = 'changeColorRed';
+    borderColor.style.animationDuration = '2s';
+    inputsColor.forEach((input) => {
+      // eslint-disable-next-line no-param-reassign
+      input.style.borderColor = '#FF0000';
+      // eslint-disable-next-line no-param-reassign
+      input.style.animationName = 'changeColorRed';
+      // eslint-disable-next-line no-param-reassign
+      input.style.animationDuration = '2s';
+    });
+    anime({
+      targets: '.wrapper',
+      easing: 'linear',
+      duration: 150,
+      translateX: [{ value: 50 }, { value: -50 }],
+    });
+  }
+
+
 }
 
 export default LoginPage;
