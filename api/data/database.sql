e8decccdc82b782e7457855650dfbe2647b41610
xavier-massart-vinci <xavier.massart@student.vinci.be>
2023-11-20 19:13:39 +0100

    fixing bug(date)

--- api/data/database.sql
+++ api/data/database.sql
@@ -9,14 +9,15 @@ CREATE TABLE IF NOT EXISTS projet.users(
 CREATE TABLE IF NOT EXISTS projet.scores(
     id_user INTEGER REFERENCES projet.users(id_user) PRIMARY KEY ,
     score INTEGER NOT NULL,
+    score_date DATE NOT NULL DEFAULT CURRENT_DATE
     CHECK ( score >= 0 )
 );
 
 CREATE OR REPLACE VIEW  projet.display_scores AS
-    SELECT u.username, s.score
+    SELECT u.username, s.score, s.score_date
     FROM projet.scores s, projet.users u
     WHERE u.id_user = s.id_user
-    ORDER BY 2 DESC;
+    ORDER BY 2 DESC, 3 DESC;
 
 CREATE OR REPLACE FUNCTION projet.user_change_score(
     _user INT,
@@ -38,10 +39,14 @@ $$ LANGUAGE plpgsql;
 
 
 
-/*INSERT INTO projet.users (username, password) VALUES ('xavier.massart', 'mdp1');
-INSERT INTO projet.users (username, password) VALUES ('thibaut.devos', 'mdp2');
+/*INSERT INTO projet.users (username, password) VALUES ('GoldKing', 'mdp1');
+INSERT INTO projet.users (username, password) VALUES ('WitcherGood', 'mdp1');
+INSERT INTO projet.users (username, password) VALUES ('WarSteel', 'mdp1');
+INSERT INTO projet.users (username, password) VALUES ('RushRonin', 'mdp1');
 
-SELECT projet.user_change_score(1, 120);*/
-
+SELECT projet.user_change_score(1, 120);
+SELECT projet.user_change_score(2, 300);
+SELECT projet.user_change_score(3, 120);
+SELECT projet.user_change_score(4, 150);*/
 
 
--- frontend/src/Components/Pages/LeaderboardPage.js
+++ frontend/src/Components/Pages/LeaderboardPage.js
@@ -1,31 +1,43 @@
 
 import Navigate from '../Router/Navigate';
+import { clearPage } from '../../utils/render';
+
+const main = document.querySelector('main');
 
 const Leaderboard = () => {
+  clearPage();
+  renderBackBtn();
   fetchAllScores();
 }
 
 
 function fetchAllScores() {
   let lignes = '';
+  
   fetch('http://localhost:3000/scores/')
   .then((response) => {
     if (!response.ok) throw new Error(`fetch error : ${response.status} : ${response.statusText}`);
     return response.json();
   })
   .then((allLigne) => {
-    let count = 1;
-    allLigne.forEach(ligne => {
-      lignes = ` 
-      <tr>
-          <td class="text-center">${count}</td>
-          <td class="text-center">${ligne.username}</td>
-          <td class="text-center">${ligne.score} m</td>
-          <td class="text-center">10/10/1999</td>
-      </tr>`;
-      count+=1;
-    });
-    renderLeaderboardPage(lignes);
+    if(allLigne.length !== 0){
+      let count = 1;
+      allLigne.forEach(ligne => {
+        const date = new Date(ligne.score_date);
+        lignes += ` 
+        <tr>
+            <td class="text-center">${count}</td>
+            <td class="text-center">${ligne.username}</td>
+            <td class="text-center">${ligne.score} m</td>
+            <td class="text-center">${date.toLocaleDateString("fr-BE")}</td>
+        </tr>`;
+        count+=1;
+      });
+      renderLeaderboardPage(lignes);
+    }else{
+      document.querySelector('main').innerHTML = `Error: no data in DB`;
+    }
+    
   })
   .catch((err) => {
     console.error('Error fetching ', err);
@@ -37,32 +49,33 @@ function fetchAllScores() {
 
 
 function renderLeaderboardPage(lignes) {
-  const main = document.querySelector('main');
-
-  main.innerHTML = `
-  <div class="d-flex justify-content-end">
-    <button type="button" class="back btn btn-primary btn-lg mx-5 my-4">Back</button>
-  </div>
- 
-
+  main.innerHTML += ` 
   <div class="d-flex justify-content-center">
     <div class="container p-2 border rounded">
-        <h2 class="text-center mb-2">LeaderBoard</h2>
+        <h2 class="text-center my-2">LeaderBoard</h2>
         <div class="table-responsive mx-4">
           <table class="table table-striped mx-auto">
-            <tr>
-              <th class="text-center">n°</th>
-              <th class="text-center">username</th>
-              <th class="text-center">score</th>
-              <th class="text-center">date</th>
-            </tr>
+          <tr>
+            <th class="text-center">n°</th>
+            <th class="text-center">username</th>
+            <th class="text-center">score</th>
+            <th class="text-center">date</th>
+          </tr>
            ${lignes}
           </table>
         </div>
     </div>
   </div>
   `;
-  backListenner();
+  
+}
+
+function renderBackBtn() {
+  main.innerHTML += `
+  <div class="d-flex justify-content-end">
+    <button type="button" class="back btn btn-primary btn-lg mx-5 my-4">Back</button>
+  </div>`;
+backListenner();
 }
 
 function backListenner() {
