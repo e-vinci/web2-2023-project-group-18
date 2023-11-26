9841d7b5eee76556067866524683f732873feaaa
Chisom <chisom.ubah@student.vinci.be>
2023-11-26 13:24:24 +0100

    add : RGPD-private-policy

--- frontend/src/Components/Router/routes.js
+++ frontend/src/Components/Router/routes.js
@@ -4,6 +4,7 @@ import Leaderboard from '../Pages/LeaderboardPage';
 import LoginPage from '../Pages/LoginPage';
 import RegisterPage from '../Pages/RegisterPage';
 import CreditsPage from '../Pages/CreditsPage'
+import PrivatePolicyPage from '../Pages/PrivatePolicyPage';
 
 
 const routes = {
@@ -13,6 +14,7 @@ const routes = {
   '/login':LoginPage,
   '/game': GamePage,
   '/leaderboard': Leaderboard,
+  '/private-policy':PrivatePolicyPage,
 };
 
 export default routes;
