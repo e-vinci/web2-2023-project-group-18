3bb641035f812933696d1ebdcf523088edeab42c
xavier-massart-vinci <xavier.massart@student.vinci.be>
2023-12-12 21:14:42 +0100

    loader

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,16 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './stylesheets/main.css';
 
+
+
 import Router from './Components/Router/Router';
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const loaderWrapper = document.getElementById('loader-wrapper');
+    loaderWrapper.style.display = 'none';
+  });
+  
 
 Router();
 
