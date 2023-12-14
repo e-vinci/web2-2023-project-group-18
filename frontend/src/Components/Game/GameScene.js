72c94fa599239f62dd020cc5400179f65aef1bc8
Chisom <chisom.ubah@student.vinci.be>
2023-12-14 21:12:52 +0100

    fix pause menu

--- frontend/src/Components/Game/GameScene.js
+++ frontend/src/Components/Game/GameScene.js
@@ -100,9 +100,9 @@ class GameScene extends Phaser.Scene {
              this.cameras.main.startFollow(this.santa);
              this.key = this.input.keyboard.addKey(localStorage.getItem('selectedKey'));
 
-             this.caracterSpeed = 3;
+             this.caracterSpeed = 5;
              setInterval(() => {
-               this.caracterSpeed += Math.log(2) / 1000;
+               this.caracterSpeed += Math.log(2) / 100;
              }, 2000);
              
              this.scorePauseScene.pauseButton.on('pointerdown', () => {
@@ -289,13 +289,17 @@ class GameScene extends Phaser.Scene {
       localStorage.removeItem('resume');
     }
 
-    if (this.cursors.space.isDown) this.santa.play('player-jump', true);
+    if (this.cursors.space.isDown)  this.santa.play('player-jump', true) ;
 
     if (this.isTouchingGround && spaceJustPressed) {
       this.santa.play('player-jump', true);
       this.santa.setVelocityY(-13);
       this.santa.setVelocityX(2*this.caracterSpeed);
       this.isTouchingGround = false;
+    }
+
+    if (this.cursors.right.isDown) {
+      this.santa.play('player-run',true);
     }
 
     const key = Settings.getKey();
@@ -348,6 +352,9 @@ class GameScene extends Phaser.Scene {
       }
     });
 
+    if (!this.scene.isActive('pause-menu')) {
+      this.scene.setActive(true,'pause-menu');
+    }
  }
 
   createDudeAnimations() {
@@ -357,6 +364,7 @@ class GameScene extends Phaser.Scene {
     });
 
     // run animation
+    
     this.anims.create({
       key: 'player-run',
       frameRate: 5,
@@ -381,6 +389,8 @@ class GameScene extends Phaser.Scene {
       }),
       repeat: -1,
     });
+
+    
 
     // jump animation
     this.anims.create({
