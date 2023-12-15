a552c500309fff81e9e18a595489205dc45724cf
xavier-massart-vinci <xavier.massart@student.vinci.be>
2023-12-15 20:59:10 +0100

    fix: remove loader

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,12 +5,5 @@ import './stylesheets/main.css';
 
 import Router from './Components/Router/Router';
 
-
-document.addEventListener('DOMContentLoaded', () => {
-    const loaderWrapper = document.getElementById('loader-wrapper');
-    loaderWrapper.style.display = 'none';
-  });
-  
-
 Router();
 
